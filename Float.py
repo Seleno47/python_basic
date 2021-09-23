cef2511fda2c27a714320b94567cfb271dd5fb9f
Lenoame <seleno1104@gmail.com>
2021-09-23 23:31:46 +0900

    Real Number_py

--- Float.py
+++ Float.py
@@ -3,6 +3,9 @@
 #소수부가 0이거나, 정수부가 0인 소수는 0을 생략하고 작성할 수 있다.
 
 #양의 실수
+from typing_extensions import runtime
+
+
 a = 157.93
 print(a)
 
@@ -17,3 +20,33 @@ print(c)
 #정수부가 0일 때 0을 생략
 d = -.7
 print(d)
+
+# 10억의 지수 표현 방식
+a = 1e9
+print(a)
+
+# 752.5
+a = 75.25e1
+print(a)
+
+# 3.954
+a = 3954e-3
+print(a)
+
+a = 0.3 + 0.6
+print(a)
+
+if a == 0.9:
+    print(True)
+else:
+    print(False)
+
+a = 0.3 + 0.6
+print(round(a, 4))
+
+if round(a, 4) == 0.9:
+    print(True)
+else:
+    print(False)
+
+
